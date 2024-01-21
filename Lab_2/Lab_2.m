e3078e188c683ef472ee236ee8d1b6283788ad45
Dyusekeev Stepan <stepandus0502@gmail.com>
2024-01-21 11:18:13 +0300

    imnoise2 function

--- Lab_2/Lab_2.m
+++ Lab_2/Lab_2.m
@@ -1,1 +1,13 @@
 clear;
+% Вариант 6
+
+%% 1. Синтезировать пустое полутновое изображение.
+blankimage = uint8(zeros(800, 800));
+% imshow(blankimage);
+
+%% 2. Нанести на изображение шум
+noise = im2uint8(imnoise2('uniform', 800, 800));
+noiseimage = blankimage + noise;
+% imshow(noiseimage);
+
+%% 3. Построить гистограмму распределения полученного шума
