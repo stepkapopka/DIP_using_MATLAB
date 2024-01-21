f02af03517aff5ddb7767a9ef3cf62f4a6c4d681
Dyusekeev Stepan <stepandus0502@gmail.com>
2024-01-21 13:54:43 +0300

    finished 3

--- Lab_5/Lab_5.m
+++ Lab_5/Lab_5.m
@@ -16,15 +16,14 @@ subplot(2,2,1); imshow(I1); title('Original image');
 subplot(2,2,2); imshow(I2); title('Opening');
 subplot(2,2,3); imshow(I3);  title('Closing');
 subplot(2,2,4); imshow(I4); title('Opening followed by closing'); pause;
-
 % Результат сохранен в файл 'result1.jpg'
 
 %% 2. Разделение объектов.
 % Выбрать произвольное бинарное изображение, 
 % содержащее перекрывающиеся объекты. 
-% 2.1 Использовать операции бинарной морфологии для разделения объектов. 
 clear;
 
+% 2.1 Использовать операции бинарной морфологии для разделения объектов. 
 I = imread('connected_elements.png');
 I1 = im2bw(I);
 I2 = ~I1;
@@ -36,7 +35,6 @@ BW2 = bwmorph (BW2, 'thicken', Inf);
 subplot(2,2,3); imshow(BW2); title('Thickened objects');
 Inew = ~(I1&BW2);
 subplot(2,2,4); imshow(Inew); title('Result'); pause;
-
 % Результат сохранен в файл 'result2.1.jpg'
 
 % 2.2 Выделить контуры объектов.
@@ -44,7 +42,6 @@ B = strel('disk', 1);
 I3 = imdilate(Inew, B);
 C = I3 & ~Inew;
 subplot(1,1,1); imshow(C); pause;
-
 % Результат сохранен в файл 'result2.2.jpg'
 
 %% 3. Сегментация.
@@ -53,6 +50,7 @@ subplot(1,1,1); imshow(C); pause;
 % Выполнить сегментацию изображения по водоразделам.
 clear;
 
+% 3.1 Фильтрация изображения
 rgb = imread ('lemons.png');
 A = rgb2gray(rgb);
 B = strel ('disk', 100);
@@ -61,12 +59,14 @@ Cr = imreconstruct (C, A);
 Crd = imdilate(Cr, B);
 Crdr = imreconstruct (imcomplement (Crd) , imcomplement (Cr));
 Crdr = imcomplement (Crdr);
-imshow(Crdr); pause;
+imshow(Crdr); title('Отфильтрованное изображение'); pause; 
+% Результат сохранен в файл 'result3.1.jpg'
 
+% 3.2 Маркеры переднего плана
 fgm = imregionalmax(Crdr);
 A2 = A;
 A2(fgm) = 255;
-imshow(A2); pause;
+subplot(1, 2, 1); imshow(A2); title('Маркеры переднего плана');
 
 B2 = strel(ones(25, 25));
 fgm = imclose(fgm, B2);
@@ -74,12 +74,39 @@ fgm = imerode(fgm, B2);
 fgm = bwareaopen(fgm, 20);
 A3 = A;
 A3(fgm) = 255;
-imshow(A3); pause;
+subplot(1, 2, 2); imshow(A3); title('Отфильтрованные маркеры переднего плана');
+pause;
+% Результат сохранен в файл 'result3.2.jpg'
 
-bw = imbinarize (Crdr);
-imshow(bw); pause;
+% 3.3 Маркеры фона
+bw = imbinarize(Crdr);
+subplot(1, 2, 1); imshow(bw); title('Бинаризованное изображение');
 D = bwdist(bw);
 L = watershed(D);
 bgm = L == 0;
-imshow(bgm); pause;
+subplot(1, 2, 2); imshow(bgm); title('Маркеры фона'); pause;
+% Результат сохранен в файл 'result3.3.jpg'
+
+% 3.4 Градиентное представление изображения
+hy = fspecial('sobel');
+hx = hy';
+Ay = imfilter(im2double(A), hy, 'replicate');
+Ax = imfilter(im2double(A), hx, 'replicate');
+grad = sqrt(Ax.^2 + Ay.^2);
+subplot(1, 2, 1); imshow(grad); title('Исходное');
+grad = imimposemin(grad, bgm | fgm );
+subplot(1, 2, 2); imshow(grad); title('Модифицированное'); pause;
+% Результат сохранен в файл 'result3.4.jpg'
+
+% 3.5 Сегментации уточненного градиентного представления
+L = watershed(grad);
+A4 = A;
+A4(imdilate (L == 0 , ones(3, 3)) | bgm | fgm ) = 255;
+subplot(1, 2, 1); imshow(A4); ...
+    title('Маркеры и границы, наложенные на исходное изображение');
+
+Lrgb = label2rgb(L , 'jet' , 'w' , 'shuffle');
+subplot(1, 2, 2); imshow(Lrgb); ...
+    title('Представленное в цветах rgb');
+% Результат сохранен в файл 'result3.5.jpg'
 
